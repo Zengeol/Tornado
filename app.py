76da833e717e71ea5732178c8a71743fb9ba075c
Zengeol <claudia.ling1128@gmail.com>
2018-03-24 12:17:03 -0500

    deployment

--- app.py
+++ app.py
@@ -12,9 +12,10 @@ def make_app():# this URL matches with this handle
     return tornado.web.Application([
         (r"/", MainHandler), #if it doesn't find the page, it will give us 404 message.
     ], autoreload=True)
-    
+
 if __name__ == "__main__":
     app = make_app()
     PORT = int(os.environ.get('PORT','9999'))
+    # mine = os.environ.get('database') to make something private, I create a "database" in Heroku, then get it overhere. 
     app.listen(PORT) # post it on port 8888
     tornado.ioloop.IOLoop.current().start()
